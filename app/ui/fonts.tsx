0ce5718cde63c446b2bb00fdc4184b6ae0f7d4ea
nguyentt1503@gmail.com <nguyentt1503@gmail.com>
2024-03-12 15:23:34 +0700

    add

---
+++ app/ui/fonts.tsx
@@ -0,0 +1,5 @@
+import {Inter} from 'next/font/google';
+import {Lunasima} from "next/font/google";
+
+export const inter = Inter({subsets: ['latin']});
+export const lunasima = Lunasima({weight: "400", subsets: ['latin']})
